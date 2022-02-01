5d44e4e979413cfe09b7a274797347ecbfea0e4a
AshishAcharya083 <imashish083@gmail.com>
2022-02-01 23:27:53 +0545

    PROBLEM: whole sheet is draggable

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:valo_stats/core/services/screen_size.dart';
 import 'package:valo_stats/ui/components/grey_white_text_comp.dart';
 import 'package:valo_stats/ui/pages/drawer.dart';
@@ -20,6 +21,7 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(15.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             RichText(
               text: TextSpan(
@@ -55,81 +57,106 @@ class HomePage extends StatelessWidget {
                   color: Colors.white),
             ),
             Container(
-              /// large container of light grey color
-              decoration: BoxDecoration(color: kLightGrayColor),
-              width: ScreenSize.getScreenWidth(context),
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Container(
-                    /// container of dark blue color containg name of the player
-                    padding: EdgeInsets.all(20),
-                    height: ScreenSize.getScreenHeight(context) * 0.3,
-                    decoration: BoxDecoration(
-                      color: kDarkBlueGrayColor,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: "rian",
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              TextSpan(
-                                text: " #ninho",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w300,
-                                  fontSize: 24,
-                                  letterSpacing: 1.2,
-                                  color: kGreyTextColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        /// rank icon and rating kd win rate
-                        Row(
+              height: 500,
+              child: DraggableScrollableSheet(
+                snap: true,
+                initialChildSize: 0.5,
+                minChildSize: 0.4,
+                maxChildSize: 0.8,
+                expand: true,
+                builder:
+                    (BuildContext context, ScrollController scrollController) {
+                  return ListView(
+                    controller: scrollController,
+                    children: [
+                      Container(
+                        /// large container of light grey color
+                        decoration: BoxDecoration(color: kLightGrayColor),
+                        width: ScreenSize.getScreenWidth(context),
+                        padding: EdgeInsets.all(20),
+                        child: Column(
                           children: [
-                            Expanded(
-                              child: Image(
-                                image: AssetImage("images/rank/gold1.png"),
+                            Container(
+                              /// container of dark blue color containg name of the player
+                              padding: EdgeInsets.all(20),
+                              height: ScreenSize.getScreenHeight(context) * 0.3,
+                              decoration: BoxDecoration(
+                                color: kDarkBlueGrayColor,
                               ),
-                            ),
-                            Expanded(
-                              flex: 2,
                               child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  GreyWhiteTextComponent(
-                                      "Rating", "platinum 3"),
-                                  GreyWhiteTextComponent("KAD Ratio", "1.42"),
-                                  GreyWhiteTextComponent("Win Rate", "46.1 %"),
+                                  RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: "rian",
+                                          style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: " #ninho",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 24,
+                                            letterSpacing: 1.2,
+                                            color: kGreyTextColor,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+
+                                  /// rank icon and rating kd win rate
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Image(
+                                          image: AssetImage(
+                                              "images/rank/gold1.png"),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            GreyWhiteTextComponent(
+                                                "Rating", "platinum 3"),
+                                            GreyWhiteTextComponent(
+                                                "KAD Ratio", "1.42"),
+                                            GreyWhiteTextComponent(
+                                                "Win Rate", "46.1 %"),
+                                          ],
+                                        ),
+                                      )
+                                      // Image.asset('images/rank_icon/diamond1.png'),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      GreyWhiteTextComponent(
+                                          "Play Time", "245H 16M"),
+                                      GreyWhiteTextComponent("Matches", "423"),
+                                    ],
+                                  ),
                                 ],
                               ),
                             )
-                            // Image.asset('images/rank_icon/diamond1.png'),
                           ],
                         ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            GreyWhiteTextComponent("Play Time", "245H 16M"),
-                            GreyWhiteTextComponent("Matches", "423"),
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                      )
+                    ],
+                  );
+                },
               ),
             ),
           ],
